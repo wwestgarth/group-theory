46504e2ab228a39f892ddd40082c95cc8bd98c89
William Westgarth <william.westgarth@ncipher.com>
2020-08-28 11:18:12 +0100

    add probably-prime test ready for testing for subgroups based on order

---
+++ groups/prime.go
@@ -0,0 +1,46 @@
+package groups
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// fermatsProbablyPrimeTest a crude probably prime test for n. For a randomly chosen a < n ,
+// if a^(n-1) != 1 (mod n)  => n is composite
+func fermatsProbablyPrimeTest(n int) bool {
+
+	a := rand.Intn(n)
+	res := a
+	for i := 2; i < n; i++ {
+		res = (res * a) % n
+	}
+
+	return res == 1
+
+}
+
+// ProbablyPrime returns where the given value is, statistically, likely to be prime
+func ProbablyPrime(n, samples int) bool {
+
+	if n < 1 {
+		panic(fmt.Sprintf("%v is not a > 1", n))
+	}
+
+	// Easy and quick to compute
+	if n == 2 {
+		return true
+	}
+
+	if n%2 == 0 {
+		return false
+	}
+
+	for i := 0; i < samples; i++ {
+		if !fermatsProbablyPrimeTest(n) {
+			return false // Definitely compostie
+		}
+	}
+
+	// Could not determine if composite, probably prime
+	return true
+}
---
+++ groups/prime_test.go
@@ -0,0 +1,36 @@
+package groups
+
+import "testing"
+
+func TestIsntPrime(t *testing.T) {
+
+	testValues := []int{9, 15, 27, 6319}
+	for _, testValue := range testValues {
+		if ProbablyPrime(testValue, 10) {
+			t.Errorf("Expected %d to be composite", testValue)
+		}
+	}
+
+}
+
+func TestIsPrime(t *testing.T) {
+
+	testValues := []int{11, 23, 313, 2017, 7901}
+	for _, testValue := range testValues {
+		if !ProbablyPrime(testValue, 10) {
+			t.Errorf("Expected %d to be prime", testValue)
+		}
+	}
+}
+
+func TestEvens(t *testing.T) {
+
+	testValues := []int{4, 6, 122, 1024}
+
+	for _, testValue := range testValues {
+		if ProbablyPrime(testValue, 10) {
+			t.Errorf("Expected %d to be composite", testValue)
+		}
+	}
+
+}
